acab51fe5fe7d8d5f52610c43fa8d5a504381699
LeConsulat2 <139603040+LeConsulat2@users.noreply.github.com>
2024-01-06 23:14:54 +1300

    Update test.py

--- test.py
+++ test.py
@@ -5,3 +5,14 @@ if password_correct:
 else:
     print ("You are a thief")    
 
+# separate code
+winner = 10
+
+if winner > 10:
+  print("winner is greater than 10")
+elif winner  < 10:
+  print("winner is less than 10")
+else:
+  print("winner is 10")
+# separate code
+
